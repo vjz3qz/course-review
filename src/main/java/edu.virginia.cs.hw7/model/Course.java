ad11146d9263e2b5ffe44186775d8e88cb009b6b
avish212 <124730965+avish212@users.noreply.github.com>
2023-05-02 00:24:59 -0400

    Updated Student and Course Class
    according to specifications in the write up

--- src/main/java/edu.virginia.cs.hw7/model/Course.java
+++ src/main/java/edu.virginia.cs.hw7/model/Course.java
@@ -1,4 +1,53 @@
 package edu.virginia.cs.hw7.model;
 
 public class Course {
+
+    //Specifications
+    //Courses: note that these fields are only for a course, not individual sections.
+    //An id number [Auto increment primary key]
+    //Department (such as "CS")
+    //Catalog_Number (such as "3140")
+    private int id;
+    private String department;
+    private String catalogNumber;
+
+    public Course(int id, String department, String catalogNumber) {
+        this.id = id;
+        this.department = department;
+        this.catalogNumber = catalogNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getCatalogNumber() {
+        return catalogNumber;
+    }
+
+    public void setCatalogNumber(String catalogNumber) {
+        this.catalogNumber = catalogNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", department='" + department + '\'' +
+                ", catalogNumber='" + catalogNumber + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/edu.virginia.cs.hw7/model/Student.java
+++ src/main/java/edu.virginia.cs.hw7/model/Student.java
@@ -1,4 +1,56 @@
 package edu.virginia.cs.hw7.model;
 
 public class Student {
+
+    //Specifications:
+//    Students containing the following
+//    An id number [Auto increment primary key]
+//    A name used for login (could be computing ID) - must be unique
+//    A password - a note that you'll be saving the password as plain text, but this is generally "bad practice" - this was simplified because handling that security is beyond the scope of this course.
+
+
+    private int id;
+    private String computingId;
+    private String password;
+
+    public Student(int id, String computingId, String password) {
+        this.id = id;
+        this.computingId = computingId;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComputingId() {
+        return computingId;
+    }
+
+    public void setComputingId(String computingId) {
+        this.computingId = computingId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", computingId='" + computingId + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
+
+
